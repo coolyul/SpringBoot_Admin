c0580779bcdab714cc5dc17c225265a2a909c1c1
coolyul <coolyul92@gmail.com>
2020-11-08 15:52:40 +0900

    SpringBoot를 이용한 Admin Project

--- eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.co.fastcampus.eatgo.domain;
-
-import java.util.List;
-
-public interface MenuItemRepository {
-    List<MenuItem> findAllByRestaurantId(Long restaurantId);
-
-}
--- eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.fastcampus.eatgo.domain;
-
-import java.util.List;
-
-public interface RestaurantRepository {
-    List<Restaurant> findAll();
-
-    Restaurant findById(Long id);
-}
